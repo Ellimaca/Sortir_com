75cdc6e2c6856c5d2bb5feed2e5b16a9a3efa208
Benou <prado.benjamin@hotmail.com>
2021-04-06 18:30:36 +0200

    Fixturlututu

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -25,11 +25,10 @@ class AppFixtures extends Fixture
             $status->setName($name);
             $manager->persist($status);
         }
+        $manager->flush();
 
         $statusRepository = $manager->getRepository(Status::class);
         $allStatus = $statusRepository->findAll();
-
-        $manager->flush();
 
         // Tableau qui regroupe les numéros des départements du grand ouest
         $westernRegions = [14, 22, 29, 35, 44, 49, 50, 56, 61, 72, 85];
@@ -40,28 +39,30 @@ class AppFixtures extends Fixture
             $city->setName($faker->city);
             $city->setPostCode($faker->randomElement($westernRegions) . $faker->numberBetween(10, 99) * 10);
             $manager->persist($city);
+
         }
+        $manager->flush();
+
 
         $cityRepository = $manager->getRepository(City::class);
         $allCity = $cityRepository->findAll();
 
-        $manager->flush();
 
         // Génèration de données aléatoires pour l'entité Place
         for ($i = 0; $i < 50; $i++) {
             $place = new Place();
-            $place->setName($faker->title);
+            $place->setName($faker->sentence);
             $place->setStreet($faker->streetAddress);
             $place->setLatitude($faker->latitude);
             $place->setLongitude($faker->longitude);
             $place->setCity($faker->randomElement($allCity));
             $manager->persist($place);
         }
+        $manager->flush();
 
         $placeRepository = $manager->getRepository(Place::class);
         $allPlaces = $placeRepository->findAll();
 
-        $manager->flush();
 
         // Génèration de données aléatoires pour l'entité Campus
         for ($i = 0; $i < 5; $i++) {
@@ -69,36 +70,38 @@ class AppFixtures extends Fixture
             $campus->setName($faker->city);
             $manager->persist($campus);
         }
+        $manager->flush();
 
         $campusRepository = $manager->getRepository(Campus::class);
         $allCampus = $campusRepository->findAll();
 
-        $manager->flush();
-
         // Génèration de données aléatoires pour l'entité user
         for ($i = 0; $i < 100; $i++) {
             $user = new User();
-            $user->setFirstName($faker->lastName);
+            $user->setFirstName($faker->firstName);
             $user->setEmail($faker->email);
-            $user->setLastName($faker->firstName);
+            $user->setLastName($faker->lastName);
             $user->setCampus($faker->randomElement($allCampus));
-            $user->setPhoneNumber($faker->numberBetween(33600000000, 33700000000));
+            $user->setPhoneNumber($faker->numberBetween(33600000, 33700000));
             $user->setPassword($faker->password);
             $user->setRoles(["ROLE_USER"]);
             $user->setIsActive(true);
             $user->setIsAdmin(false);
+
             $manager->persist($user);
         }
+        $manager->flush();
 
         $userRepository = $manager->getRepository(User::class);
         $allUsers = $userRepository->findAll();
 
-        $manager->flush();
+
+        // TODO vérification date
 
         // Génèration de données aléatoires pour l'entité Event
         for ($i = 0; $i < 100; $i++) {
             $event = new Event();
-            $event->setName($faker->title);
+            $event->setName($faker->sentence);
             $event->setDateTimeStart($faker->dateTimeBetween(" - 1 month"));
             $event->setDateTimeEnd($faker->dateTimeBetween($event->getDateTimeStart(), "+ 2 days"));
             $event->setRegistrationDeadline($faker->dateTimeBetween($event->getDateTimeStart(), "+ 7 days"));
@@ -109,13 +112,10 @@ class AppFixtures extends Fixture
             $event->setPlace($faker->randomElement($allPlaces));
             $event->setOrganiser($faker->randomElement($allUsers));
             $manager->persist($event);
-
-
-
         }
-
         $manager->flush();
 
+        // TODO event_user
 
     }
 }
