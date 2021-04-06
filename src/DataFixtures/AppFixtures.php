cdbc79e62c748bc8227d25b8d984f676a7059147
Benou <prado.benjamin@hotmail.com>
2021-04-06 16:22:26 +0200

    Wallace and Commit 2

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,11 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Campus;
+use App\Entity\City;
+use App\Entity\Event;
+use App\Entity\Place;
+use App\Entity\Status;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +14,83 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // On utilise Faker avec des données aléatoires en français
+        $faker = \Faker\Factory::create("fr_FR");
+
+        // Le nom des différents états possibles pour une sortie
+        $statusName = ["Créée", "Ouverte", "Clôturée", "Activité en cours", "Passée", "Annulée"];
+        foreach($statusName as $name){
+            $status = new Status();
+            $status->setName($statusName);
+            $manager->persist($status);
+        }
+
+        $statusRepository = $manager->getRepository(Status::class);
+        $allStatus = $statusRepository->findAll();
 
         $manager->flush();
+
+        // Tableau qui regroupe les numéros des départements du grand ouest
+        $westernRegions = [14, 22, 29, 35, 44, 49, 50, 56, 61, 72, 85];
+
+        // Génèration de données aléatoires pour l'entité City
+        for ($i = 0; $i<50; $i++) {
+            $city = new City();
+            $city->setName($faker->city);
+            $city->setPostCode($faker->randomElement($westernRegions) . $faker->numberBetween(10, 99)*10);
+        }
+
+        $cityRepository = $manager->getRepository(City::class);
+        $allCity = $cityRepository->findAll();
+
+        $manager->flush();
+
+        // Génèration de données aléatoires pour l'entité Place
+        for ($i = 0; $i<50; $i++) {
+            $place = new Place();
+            $place->setName($faker->title);
+            $place->setStreet($faker->streetAddress);
+            $place->setLatitude($faker->latitude);
+            $place->setLongitude($faker->longitude);
+            $place->setCity($faker->randomElement($allCity));
+        }
+
+        $placeRepository = $manager->getRepository(Place::class);
+        $allPlaces = $placeRepository->findAll();
+
+        $manager->flush();
+
+        // Génèration de données aléatoires pour l'entité Campus
+        for ($i = 0; $i<5; $i++) {
+            $campus = new Campus();
+            $campus->setName($faker->city);
+        }
+
+        $campusRepository = $manager->getRepository(Campus::class);
+        $allCampus = $campusRepository->findAll();
+
+        $manager->flush();
+
+        // Génèration de données aléatoires pour l'entité Event
+        $event = new Event();
+        $event->setName($faker->title);
+        $event->setDateTimeStart($faker->dateTimeBetween("+ 7 days ", "+ 3 months"));
+        $event->setDateTimeEnd($faker->dateTimeBetween($event->getDateTimeStart(), "+ 1 day"));
+        $event->setRegistrationDeadline($faker->dateTimeBetween($event->getDateTimeStart(), "+ 7 days"));
+        $event->setDescription($faker->paragraphs($faker->numberBetween(0, 3), true));
+        $event->setCampus($faker->randomElement($allCampus));
+        $event->setMaxNumberParticipants($faker->numberBetween(2, 100));
+        $event->setStatus($faker->randomElement($allStatus));
+        $event->setPlace($faker->randomElement($allPlaces));
+
+        // TODO
+        //$event->setOrganiser()
+        //$event->setDuree
+
+        $manager->flush();
+
+
+
+
     }
 }
