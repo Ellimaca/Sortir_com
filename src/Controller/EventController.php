fda345a60c8bfb40db75c82a3c29882058f94951
Benou <prado.benjamin@hotmail.com>
2021-04-07 16:08:32 +0200

    Event view

---
+++ src/Controller/EventController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\EventRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    /**
+     * @Route("/event", name="event")
+     */
+    public function index(): Response
+    {
+        return $this->render('event/index.html.twig', [
+            'controller_name' => 'EventController',
+        ]);
+    }
+
+    /**
+     * Fonction qui permet de visualiser les informations liées à un évenement
+     * @Route ("/evenement/{id}", name="event_view")
+     * @param int $id
+     * @param EventRepository $eventRepository
+     * @return Response
+     */
+    public function view(int $id, EventRepository $eventRepository): Response
+    {
+        $foundEvent = $eventRepository->findOneBy(['id' => $id]);
+        if (!$foundEvent) {
+            throw $this->createNotFoundException("Cet évenement n'existe pas");
+        }
+        $foundParticipants = $foundEvent->getParticipants();
+        return $this->render('event/view.html.twig', ['foundEvent' => $foundEvent, 'foundParticipants' => $foundParticipants]);
+    }
+}
