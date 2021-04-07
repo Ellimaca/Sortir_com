314caa338c7383fdb692da80365c3222cf1eab9d
Benou <prado.benjamin@hotmail.com>
2021-04-07 16:07:09 +0200

    Event view

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -14,11 +14,19 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class EventRepository extends ServiceEntityRepository
 {
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Event::class);
     }
+/**
+    public function findUsersForEvent (Event $event):array{
 
+        $qb = $this->createQueryBuilder('e');
+
+
+
+    }/*
     // /**
     //  * @return Event[] Returns an array of Event objects
     //  */
