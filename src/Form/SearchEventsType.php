ab967f145ab539431b4686083569c4c9698a9d54
Peeters-Ruffel <nguimbi.taharqa@hotmail.fr>
2021-04-08 14:02:38 +0200

    modification de l'affichage de l'accueil

--- src/Form/SearchEventsType.php
+++ src/Form/SearchEventsType.php
@@ -6,12 +6,11 @@ use App\Entity\Campus;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\ChoiceList\ChoiceList;
 
 class SearchEventsType extends AbstractType
 {
@@ -20,17 +19,14 @@ class SearchEventsType extends AbstractType
         $builder
             ->add('campus', EntityType::class, [
                 'class' => Campus::class,
-                'choice_label' => 'name'
+                'choice_label' => 'name',
+                'choice_value'  => ChoiceList::value($this, 'name')
             ])
             ->add('searchBar', TextType::class, [
                 'required' => false
             ])
-            ->add('dateStart', DateType::class, [
-                'required' => false
-            ])
-            ->add('dateEnd', DateTimeType::class, [
-                'required' => false
-            ])
+            ->add('dateStart')
+            ->add('dateEnd')
             ->add('isOrganisedByMe', CheckboxType::class, [
                 'label' => "Sorties dont je suis l'organisateur/trice",
                 'required' => false
