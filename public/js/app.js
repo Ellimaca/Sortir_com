0b0fc3695ac6687cf10ac66b0de60e603f22b2bb
Ellimaca <camille.a@live.fr>
2021-04-14 12:34:13 +0200

    Avancement sur l'affichage de l'adresse

---
+++ public/js/app.js
@@ -0,0 +1,56 @@
+function init() {
+    addListeners();
+}
+
+function addListeners() {
+
+    var city = document.getElementById('event_city');
+
+    city.addEventListener("change", updatePlaceField);
+
+    //Récupération du lieu choisi
+    var place = document.getElementById('event[place]');
+
+    place.addEventListener('change', displayInformations);
+
+}
+
+function updatePlaceField() {
+
+    let city_id = document.getElementById('event_city')
+    let data = {"eventCity": city_id.value}
+
+    console.log(data)
+
+    fetch('ajax', {method: 'POST', body: JSON.stringify(data)})
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+
+        places = data.places
+        html = "";
+
+        for (const [key, value] of Object.entries(places)) {
+            html = html + "<option value='" + `${key}` + "'>" + `${value.name}` + "</option>" + "\n";
+        }
+        document.getElementById('event[place]').innerHTML = html;
+    })
+
+    displayInformations();
+}
+
+
+function displayInformations() {
+
+    //Récupération de l'id du lieu
+    let place_id = document.getElementById('event[place]')
+
+    console.log(place_id);
+
+    let data = {'placeId' : place_id.options[place_id.selectedIndex].value}
+
+    console.log(data)
+
+}
+
+
