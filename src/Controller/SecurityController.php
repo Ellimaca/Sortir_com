daeef4c6de3295be74d4bcef50863422d3d0eced
Peeters-Ruffel <nguimbi.taharqa@hotmail.fr>
2021-04-15 17:40:07 +0200

    correction de la ville par défaut

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use App\Entity\ProfilePicture;
 use App\Entity\User;
-use App\Form\ProfilePictureType;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,6 +21,16 @@ use Symfony\Component\String\ByteString;
 
 class SecurityController extends AbstractController
 {
+    const REGEX_PASSWORD = "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$^";
+    const REGEX_PSEUDO = "^[a-zA-Z0-9]+$^";
+    //const REGEX_NAME = "^[A-Za-zàâçéèêëîïôûùüÿñæœ' -]*$^";
+    const REGEX_NAME = "^[a-zA-Z]+$^";
+    const REGEX_PHONE_NUMBER = "^(?:(?:(?:\+|00)33[ ]?(?:\(0\)[ ]?)?)|0){1}[1-9]{1}([ .-]?)(?:\d{2}\1?){3}\d{2}$^";
+
+    const WARNING_PSEUDO_CHAR_NOT_AUTHORIZED = "Seulement les lettres et les chiffres sont acceptés, pas d'accents ou de caractères spéciaux autorisés";
+    const WARNING_NAME_CHAR_NOT_AUTHORIZED = "Le format du prénom n'est pas valide. Caractères autorisés : a-z, ', -, ";
+    const WARNING_PHONE_NUMBER_CHAR_NOT_AUTHORIZED = "Veuillez entrer un format de téléphone valide (00-00-00-00-00)";
+
     /**
      * @Route("/login", name="app_login")
      * @param AuthenticationUtils $authenticationUtils
@@ -77,44 +88,69 @@ class SecurityController extends AbstractController
 
 
         //Vérification du formulaire
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted()) {
 
-            //On récupère les données de la photo
-            /** @var UploadedFile $downloadedPicture */
-            $downloadedPicture = $form->get('my_file')->getData();
+            //Vérification du pseudo
+            $this->verifyRegex($profilUser->getPseudo(),
+                self::REGEX_PSEUDO,
+                $form, 'pseudo',
+                self::WARNING_PSEUDO_CHAR_NOT_AUTHORIZED);
 
-            //Si la photo a bien été téléchargée...
-            if ($downloadedPicture) {
-                $newNamePicture = ByteString::fromRandom(30) . '.' . $downloadedPicture->guessExtension();
+            //Vérification du FirstName
+            $this->verifyRegex($profilUser->getFirstName(),
+                self::REGEX_NAME,
+                $form, 'firstName',
+                self::WARNING_NAME_CHAR_NOT_AUTHORIZED);
 
-                try {
-                    $downloadedPicture->move(__DIR__ . '/../../public/profile/img', $newNamePicture);
-                } catch (\Exception $e) {
-                    dd($e->getMessage());
+            //Vérification du LastName
+            $this->verifyRegex($profilUser->getLastName(),
+                self::REGEX_NAME,
+                $form, 'lastName',
+                self::WARNING_NAME_CHAR_NOT_AUTHORIZED);
+
+//            //Vérification du telephone
+//            $this->verifyRegex($profilUser->getPhoneNumber(),
+//                self::REGEX_PHONE_NUMBER,
+//                $form, 'phoneNumber',
+//                self::WARNING_PHONE_NUMBER_CHAR_NOT_AUTHORIZED);
+
+            if ($form->isValid()) {
+
+                //On récupère les données de la photo
+                /** @var UploadedFile $downloadedPicture */
+                $downloadedPicture = $form->get('my_file')->getData();
+
+                //Si la photo a bien été téléchargée...
+                if ($downloadedPicture) {
+                    $newNamePicture = ByteString::fromRandom(30) . '.' . $downloadedPicture->guessExtension();
+
+                    try {
+                        $downloadedPicture->move(__DIR__ . '/../../public/profile/img', $newNamePicture);
+                    } catch (\Exception $e) {
+                        dd($e->getMessage());
+                    }
+
+                    //On ajoute la photo au profil du user
+                    $profilePicture->setUser($profilUser);
+                    $profilePicture->setFileName($newNamePicture);
+
+                    $manager->persist($profilePicture);
+                    $manager->flush();
                 }
 
-                //On ajoute la photo au profil du user
-                $profilePicture->setUser($profilUser);
-                $profilePicture->setFileName($newNamePicture);
+                //Récupération des données dans le champ "Nouveau mot de passe"
+                $newPassword = $form['newPassword']->getData();
 
-                $manager->persist($profilePicture);
-                $manager->flush();
-            }
+                //si l'utilisateur ne change pas son mot de passe alors il garde le même
+                if ($newPassword == null) {
+                    $profilUser->setPassword($this->getUser()->getPassword());
+                    $manager->persist($profilUser);
+                    $manager->flush();
 
-            //Récupération des données dans le champ "Nouveau mot de passe"
-            $newPassword = $form['newPassword']->getData();
+                    $this->addFlash('success', 'Profil modifié ! ');
 
-            //si l'utilisateur ne change pas son mot de passe alors il garde le même
-            if ($newPassword == null) {
-                $profilUser->setPassword($this->getUser()->getPassword());
-                $manager->persist($profilUser);
-                $manager->flush();
-
-                $this->addFlash('success', 'Profil modifié ! ');
-
-            }
-                //si l'utilisateur change son mot de passe...
-            else {
+                } //si l'utilisateur change son mot de passe...
+                else {
                     //Vérification avec l'appel de la fonction si les contraintes sont respectées.
                     if ($this->verifyConstraintsNewPassword($newPassword)) {
                         $passwordEncoded = $passwordEncoder->encodePassword($profilUser, $newPassword);
@@ -124,6 +160,7 @@ class SecurityController extends AbstractController
                         $this->addFlash('success', 'Profil modifié ! ');
                     }
                 }
+            }
         }
 
 
@@ -142,11 +179,37 @@ class SecurityController extends AbstractController
         $isVerifiedConstraints = false;
 
         //Minimum 8 caractères, au moins une lettre et un chiffre:
-        if(!preg_match("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$^", $newPassword)) {
+        if (!preg_match(self::REGEX_PASSWORD, $newPassword)) {
             $this->addFlash('warning', 'Le mot de passe doit contenir 8 caractères minimum, au moins une lettre ET un chiffre');
             $isVerifiedConstraints = false;
         } else {
             return true;
+        }
+
+        return $isVerifiedConstraints;
+    }
+
+    /**
+     * Fonction permettant de faire les vérifications type Regex sur le nouveau mot de passe
+     * @param $newPassword
+     * @return bool
+     */
+    public function verifyRegex(string $string,
+                                string $regex,
+                                FormInterface $form = null,
+                                string $field ,
+                                string $errorMessage ): bool
+    {
+        $isVerifiedConstraints = false;
+
+        //Minimum 8 caractères, au moins une lettre et un chiffre:
+        if (preg_match($regex, $string)) {
+            $isVerifiedConstraints = true;
+        } else {
+            if ($form != null) {
+                $form->get($field)->addError(new FormError($errorMessage));
+                var_dump($field);
+            }
         }
 
         return $isVerifiedConstraints;
@@ -160,11 +223,11 @@ class SecurityController extends AbstractController
     {
         $userChoosen = $userRepository->find($id);
 
-        if (!$userChoosen){
+        if (!$userChoosen) {
             throw $this->createNotFoundException("Participant inconnu");
         }
 
-        return $this->render('participant/view.html.twig', [ 'userChoosen' => $userChoosen
+        return $this->render('participant/view.html.twig', ['userChoosen' => $userChoosen
         ]);
     }
 
